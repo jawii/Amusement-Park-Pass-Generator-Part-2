cb6fa5788ef0eabca2fde71bf365b0f6db0c8cf9
Jaakko Kenttä <kenttajaa@gmail.com>
2018-04-17 21:57:12 +0300

    Initial Commit

---
+++ Amusement Park Pass Generator: Part 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amusement Park Pass Generator: Part 2
+//
+//  Created by Jaakko Kenttä on 17/04/2018.
+//  Copyright © 2018 Jaakko Kenttä. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
